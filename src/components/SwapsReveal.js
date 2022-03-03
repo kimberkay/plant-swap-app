af283f2e44aefa83620c2293779c95a4021b258d
Kim Brannian <brannian@gmail.com>
2022-03-02 22:53:05 -0800

    flesh out swapReveal component

--- src/components/SwapsReveal.js
+++ src/components/SwapsReveal.js
@@ -15,9 +15,9 @@ const SwapsReveal = () => {
     <React.Fragment>
       <h2>Try These Plant Swaps!</h2>
      
-      { status === 'loading' && (<div>Loading...</div>)};
+      { status === 'loading' && (<div>Loading...</div>)}
 
-      { status === 'error' && (<div>Error fetching data</div>)};
+      { status === 'error' && (<div>Error fetching data</div>)}
  
       { status === 'success' && (
         
@@ -25,11 +25,16 @@ const SwapsReveal = () => {
           { data.filter(e=>e.ingredientToSwap === "chicken" && e.cuisineType === "Japanese")
             .map(data => ( 
           <li key={data.swapId}>
-            <p>{data.swapId}</p>
+            <p>Out with the {data.ingredientToSwap}...</p>
+            <p>Give these a go...</p>
             <h3>{data.swapOne}</h3>
-            <p>{data.swapOnePrep}</p>
+            <p>{data.swapOneAbout}</p>
+            <h3>{data.swapTwo}</h3>
+            <p>{data.swapTwoAbout}</p>
+            <h3>{data.swapThree}</h3>
+            <p>{data.swapThreeAbout}</p>
           </li>    
-        ))};
+        ))}
         </ul>
       )} 
     </React.Fragment> 
