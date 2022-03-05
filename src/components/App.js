9322b8fc08f0a9d92c093f2ee4292ea66bb78860
Kim Brannian <brannian@gmail.com>
2022-03-05 10:17:28 -0800

    delete, start new project set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,11 @@
-import React, { useState } from 'react';
-import { QueryClientProvider, QueryClient } from 'react-query';
-import Header from './Header';
-import Modal from './Modal'
-import SelectIngredientForm from './SelectIngredientForm';
-import SwapsReveal from './SwapsReveal';
-
-const queryClient = new QueryClient()
+import './App.css'
 
 function App() {
-  const [showModal, setShowModal]= useState(false)
-  
-const handleClick = () => {
-  return setShowModal;
-}
-
-
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <Header />
-      {showModal && (
-        <Modal>
-          <SelectIngredientForm />
-        </Modal>
-      )}
-      <div>
-        <button onClick={()=> setShowModal(true)}>Enter Ingredient to Swap</button>
-      </div>
-      <SwapsReveal />
-    </QueryClientProvider>
-  )
+    <div className="App">
+      
+    </div>
+  );
 }
 
-export default App;
+export default App
