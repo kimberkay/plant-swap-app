2162b47fd7661bfca087418c17a60c1aed10b452
Kim Brannian <brannian@gmail.com>
2022-03-05 10:38:58 -0800

    add new project structure

---
+++ src/pages/search/SelectIngredientForm.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+
+export default function SelectIngredientForm({ ingredientSelection }) {
+  const [ingredient, setIngredient] = useState('beef')
+  const [cuisine, setCuisine] = useState('american comfort')
+
+  const resetForm = () => {
+    setIngredient('')
+    setCuisine('')
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const userSelection = 
+      [ingredient,cuisine]  
+      console.log(userSelection)
+    }
+
+
+
+    
+    resetForm()
+  
+
+  return (
+    <form className="select-ingredient-form" onSubmit={handleSubmit}>
+      <label>
+        <span>Select Ingredient to Swap:</span>
+        <select onChange={(e) => setIngredient(e.target.value)}>
+          <option value="chicken">chicken</option>
+          <option value="beef">beef</option>
+          <option value="milk">milk</option>
+          <option value="cheese">cheese</option>
+          <option value="eggs">eggs</option>
+        </select>
+      </label>
+      <label>
+        <span>Select the Cuisine Type of Your Recipe:</span>
+        <select onChange={(e) => setCuisine(e.target.value)}>
+          <option value="Italian">Italian</option>
+          <option value="Japanese">Japanese</option>
+          <option value="Thai">Thai</option>
+          <option value="Chinese">Chinese</option>
+          <option value="AmericanComfort">American Comfort</option>
+          <option value="BakedGoods">baked goods</option>
+        </select>
+      </label>
+      <button>Submit</button>
+    </form>
+  )
+}
