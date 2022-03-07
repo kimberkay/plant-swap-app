8ecd1b5189115357cc33c534393615f93a633768
Kim Brannian <brannian@gmail.com>
2022-03-06 23:24:31 -0800

    finally debugged

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -19,7 +19,6 @@ export default function Home(props) {
 
   return (
     <React.Fragment>
-      {/* <Search  /> */}
       <Swaps />
     </React.Fragment>  
   )
--- src/pages/home/Swaps.js
+++ src/pages/home/Swaps.js
@@ -8,19 +8,20 @@ import './Swaps.css'
 
 export default function Swaps(swaps) {
   const { data, isPending, error } = useFetch('https://localhost:5001/api/swaps/')
-  const [addSelection, setAddSelection] = useState({})
+  const [addSelection, setAddSelection] = useState([])
+  const [showSwaps, setShowSwaps] = useState({})
 
   
   
 
   const addUserSelection = (swaps) => { 
     setAddSelection(swaps)
-    return swaps;
     console.log("here i am");
+    return swaps;
+    
   }
 
  
-
   console.log(addUserSelection, "you made it");
   console.log(1)
   
@@ -31,12 +32,12 @@ export default function Swaps(swaps) {
   return (
     <React.Fragment>
       <Search addUserSelection={addUserSelection} />
-      console.log(2);
+      console.log(2)
       console.log(addUserSelection)
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {console.log(addSelection, data)}
-        {data && data.filter(e=>e.ingredientToSwap === {addSelection} && e.cuisineType === {addSelection})
+      {console.log(addSelection, data, showSwaps)}
+        {data && data.filter(e=>e.ingredientToSwap === addSelection[0] && e.cuisineType === addSelection[1])
         .map(swap => ( 
         <li key={swap.swapId}>
           <h3>{swap.swapOne}</h3>
