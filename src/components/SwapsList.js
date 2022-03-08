c4a5c179c555ebc4ccf6b700e459d4c5e940e83a
Kim Brannian <brannian@gmail.com>
2022-03-08 09:16:59 -0800

    debug swap list

--- src/components/SwapsList.js
+++ src/components/SwapsList.js
@@ -1,22 +1,31 @@
-import { useState } from 'react'
+
 import React from 'react'
 
 export default function SwapList({ swaps }) {
+
+ 
+
+
  
   return(
     <React.Fragment>
+     { console.log(swaps) }
     <div className="swap-list">
-      {swaps.map(swap => (
-        <div key={swap.SwapId} className="card">
+    
+      {swaps.map((swap, index) => (
+        <div> 
+          <li key={index} className="card">
           <h3>{swap.swapOne}</h3>
           <h3>{swap.swapTwo}</h3>
           <h3>{swap.swapThree}</h3>
+          </li>
         </div>
       ))}
+      {console.log(swaps)}
     </div>
     </React.Fragment>
     
-
+  //key is invalid...does it need to be a string?  must be unique?
     
   )
 }
