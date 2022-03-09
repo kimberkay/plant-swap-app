cc92908696b2215a834013970f8d9231345c9155
Kim Brannian <brannian@gmail.com>
2022-03-08 16:54:01 -0800

    add routing

---
+++ src/pages/about/About.js
@@ -0,0 +1,10 @@
+import './About.css'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>The Plant Based Mindset</h1>
+      <p>text text text</p>
+    </React.Fragment>
+  )
+}
