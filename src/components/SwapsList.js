fd7b072e1f2a19734cb668d52004f64897f97e17
Kim Brannian <brannian@gmail.com>
2022-03-08 16:23:40 -0800

    edit swap cards

--- src/components/SwapsList.js
+++ src/components/SwapsList.js
@@ -13,19 +13,17 @@ export default function SwapList({ swaps }) {
     <div className="swap-list">
     
       {swaps.map((swap, index) => (
-        <div> 
-          <li key={index} className="card">
+        <div key={swap.swapId} className="card">
+          <h3>Swap: <em>{swap.ingredientToSwap}</em></h3>
+          <h3>Cuisine Type: <em>{swap.cuisineType}</em></h3>
+          <h3>Plant Swaps:</h3>
           <h3>{swap.swapOne}</h3>
           <h3>{swap.swapTwo}</h3>
           <h3>{swap.swapThree}</h3>
-          </li>
         </div>
       ))}
       {console.log(swaps)}
     </div>
-    </React.Fragment>
-    
-  //key is invalid...does it need to be a string?  must be unique?
-    
+    </React.Fragment>  
   )
 }
